f065264031da177af8ec517b629a5b804d48874c
Natalia Spiridonova <natusic199797@gmail.com>
2017-03-19 02:20:28 +0300

    configure structure

---
+++ ParentsApplication/Modules/RegistrationModule/RegistrationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationViewController.swift
+//  ParentsApplication
+//
+//  Created by Наталья on 15.03.17.
+//  Copyright © 2017 IOSLab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegistrationViewController: UIViewController {
+    
+    @IBOutlet weak var switchButton: UISwitch!
+    
+    let controller = RegistrationViewController()
+    
+    func contr(){
+        
+        
+    }
+}
